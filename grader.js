24a19a1b8cd796c5ccb58a05194ca3a08bc24640
Mike Cave <mikecave@gmail.com>
2013-07-10 21:02:38 +0000

    grader.js

--- grader.js
+++ grader.js
@@ -22,10 +22,21 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var getUrlContents = function(inurl){
+    return rest.get(inurl).on('complete',function(data){
+	//sys.puts(result);
+console.log("WE ARE HERE");	
+console.log(data);
+console.log("finished");
+	return data.message;
+    });
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -43,6 +54,19 @@ var cheerioHtmlFile = function(htmlfile) {
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
+
+var checkUrl = function(urlContents, checksfile) {
+    console.log(urlContents.toString());
+    console.log(urlContents.toString().indexOf("1"));
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = urlContents.toString().indexOf(checks[ii])!== -1;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
@@ -65,8 +89,10 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-url, --url <remote_url>','Url for remote website', clone(getUrlContents),HTMLFILE_DEFAULT) 
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    //var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson = checkUrl(program.url, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
